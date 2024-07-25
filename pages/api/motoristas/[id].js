16593796e16c6ef3071232ed2ed4fba5ba8a9b2c
Renan Medeiros <renanmedeiros79@gmail.com>
2024-07-25 18:18:37 -0300

    adicionando a api dos motoristas

---
+++ pages/api/motoristas/[id].js
@@ -0,0 +1,54 @@
+import connectToDatabase from "../../../utils/db";
+import Motorista from "../../../models/Motorista";
+
+export default async function handler(req, res) {
+  const { method } = req;
+  const { id } = req.query;
+
+  try {
+    await connectToDatabase();
+    switch (method) {
+      case "GET":
+        const motorista = await Motorista.findById(id);
+        if (!motorista)
+          return res
+            .status(404)
+            .json({ success: false, message: "Motorista não encontrado" });
+        res.status(200).json({ success: true, data: motorista });
+        break;
+
+      case "PUT":
+        const updatedMotorista = await Motorista.findByIdAndUpdate(
+          id,
+          req.body,
+          { new: true }
+        );
+        if (!updatedMotorista)
+          return res
+            .status(404)
+            .json({ success: false, message: "Motorista não encontrado" });
+        res.status(200).json({ success: true, data: updatedMotorista });
+        break;
+
+      case "DELETE":
+        const deletedMotorista = await Motorista.findByIdAndDelete(id);
+        if (!deletedMotorista)
+          return res
+            .status(404)
+            .json({ success: false, message: "Motorista não encontrado" });
+        res
+          .status(200)
+          .json({ success: true, message: "Motorista deletado com sucesso" });
+        break;
+
+      default:
+        res.setHeader("Allow", ["GET", "PUT", "DELETE"]);
+        res.status(405).end(`Método ${method} não permitido`);
+        break;
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ success: false, message: "Erro interno do servidor" });
+  }
+}
