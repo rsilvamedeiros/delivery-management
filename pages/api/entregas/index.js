968e0774225826d34baf1a9ae569ef977e1a0d90
Renan Medeiros <renanmedeiros79@gmail.com>
2024-07-25 13:50:03 -0300

    adicionando logs de método, conexão, operações e erro, melhorias para monitorar a API

--- pages/api/entregas/index.js
+++ pages/api/entregas/index.js
@@ -3,23 +3,38 @@ import Entrega from "../../../models/Entrega";
 
 export default async function handler(req, res) {
   const { method } = req;
-  await connectToDatabase();
+  console.log(`Método da requisição: ${method}`);
 
-  switch (method) {
-    case "GET":
-      const entregas = await Entrega.find({});
-      res.status(200).json({ success: true, data: entregas });
-      break;
-    case "POST":
-      try {
+  try {
+    await connectToDatabase();
+    console.log("Conectado ao banco de dados com sucesso.");
+
+    switch (method) {
+      case "GET":
+        console.log("Requisitando todas as entregas...");
+        const entregas = await Entrega.find({});
+        console.log(`Número de entregas encontradas: ${entregas.length}`);
+        res.status(200).json({ success: true, data: entregas });
+        break;
+
+      case "POST":
+        console.log("Recebendo dados para criar uma nova entrega...");
         const entrega = await Entrega.create(req.body);
+        console.log("Nova entrega criada com sucesso:", entrega);
         res.status(201).json({ success: true, data: entrega });
-      } catch (error) {
-        res.status(400).json({ success: false });
-      }
-      break;
-    default:
-      res.status(400).json({ success: false });
-      break;
+        break;
+
+      default:
+        console.log(`Método ${method} não suportado.`);
+        res
+          .status(400)
+          .json({ success: false, message: "Método não suportado" });
+        break;
+    }
+  } catch (error) {
+    console.error("Erro ao processar a requisição:", error.message);
+    res
+      .status(500)
+      .json({ success: false, message: "Erro interno do servidor" });
   }
 }
--- pages/api/vendas/index.js
+++ pages/api/vendas/index.js
@@ -3,23 +3,38 @@ import Venda from "../../../models/Venda";
 
 export default async function handler(req, res) {
   const { method } = req;
-  await connectToDatabase();
+  console.log(`Método da requisição: ${method}`);
 
-  switch (method) {
-    case "GET":
-      const vendas = await Venda.find({});
-      res.status(200).json({ success: true, data: vendas });
-      break;
-    case "POST":
-      try {
+  try {
+    await connectToDatabase();
+    console.log("Conectado ao banco de dados com sucesso.");
+
+    switch (method) {
+      case "GET":
+        console.log("Requisitando todas as vendas...");
+        const vendas = await Venda.find({});
+        console.log(`Número de vendas encontradas: ${vendas.length}`);
+        res.status(200).json({ success: true, data: vendas });
+        break;
+
+      case "POST":
+        console.log("Recebendo dados para criar uma nova venda...");
         const venda = await Venda.create(req.body);
+        console.log("Nova venda criada com sucesso:", venda);
         res.status(201).json({ success: true, data: venda });
-      } catch (error) {
-        res.status(400).json({ success: false });
-      }
-      break;
-    default:
-      res.status(400).json({ success: false });
-      break;
+        break;
+
+      default:
+        console.log(`Método ${method} não suportado.`);
+        res
+          .status(400)
+          .json({ success: false, message: "Método não suportado" });
+        break;
+    }
+  } catch (error) {
+    console.error("Erro ao processar a requisição:", error.message);
+    res
+      .status(500)
+      .json({ success: false, message: "Erro interno do servidor" });
   }
 }
