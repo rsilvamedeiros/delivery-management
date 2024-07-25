77e72eb1d305b8e02cb954427e8b5eafda387f00
Renan Medeiros <renanmedeiros79@gmail.com>
2024-07-25 18:14:43 -0300

    adicionando o model de Marca.js

---
+++ models/Marca.js
@@ -0,0 +1,11 @@
+// models/Marca.js
+import mongoose from "mongoose";
+
+const MarcaSchema = new mongoose.Schema({
+  nome: {
+    type: String,
+    required: [true, "Nome da marca é obrigatório"],
+  },
+});
+
+export default mongoose.models.Marca || mongoose.model("Marca", MarcaSchema);
