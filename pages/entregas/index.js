ab199a2f04e59e519187173fa314fb31c2e5966c
Renan Medeiros <renanmedeiros79@gmail.com>
2024-07-25 07:31:15 -0300

    Nova page para exibir a lista de todas as entregas

---
+++ pages/entregas/index.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Link from "next/link";
+
+export default function Entregas() {
+  const [entregas, setEntregas] = useState([]);
+
+  useEffect(() => {
+    async function fetchEntregas() {
+      const response = await axios.get("/api/entregas");
+      setEntregas(response.data.data);
+    }
+    fetchEntregas();
+  }, []);
+
+  return (
+    <div>
+      <h1>Lista de Entregas</h1>
+      <Link href="/entregas/nova">
+        <a>Adicionar Nova Entrega</a>
+      </Link>
+      <ul>
+        {entregas.map((entrega) => (
+          <li key={entrega._id}>
+            <Link href={`/entregas/${entrega._id}`}>
+              <a>
+                {entrega.endereco} - {entrega.status}
+              </a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
