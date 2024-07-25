14d7e75a6b1c3b3ec2b9a5e288b4e839dfa0ef82
Renan Medeiros <renanmedeiros79@gmail.com>
2024-07-25 13:46:49 -0300

    ajustes no db.js (validação da conexão ao banco mongoDB)

--- utils/db.js
+++ utils/db.js
@@ -5,10 +5,10 @@ import mongoose from "mongoose";
 
 const MONGO_URI = process.env.MONGO_URI;
 
-console.log("MONGO_URI in db.js:", MONGO_URI); // Adicione este log para depuração
+console.log("MONGO_URI no db.js:", MONGO_URI); // Log para depuração
 
 if (!MONGO_URI) {
-  throw new Error("Please add your Mongo URI to .env");
+  throw new Error("Por favor, adicione sua URI do MongoDB ao arquivo .env");
 }
 
 let cached = global.mongoose;
@@ -19,17 +19,22 @@ if (!cached) {
 
 async function connectToDatabase() {
   if (cached.conn) {
+    console.log("Usando a conexão de banco de dados existente.");
     return cached.conn;
   }
 
   if (!cached.promise) {
+    console.log("Iniciando a conexão com o MongoDB...");
+
     cached.promise = mongoose
-      .connect(MONGO_URI)
+      .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
       .then((mongoose) => {
+        console.log("Conectado ao MongoDB com sucesso!");
         return mongoose;
       })
       .catch((error) => {
-        console.error("Error connecting to MongoDB:", error);
+        console.error("Erro ao conectar ao MongoDB:", error.message);
+        throw error; // Re-throw the error after logging it
       });
   }
 
